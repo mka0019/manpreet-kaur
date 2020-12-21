b60f93eea741f9f3bd1001898ec193157af7aaa3
Manpreet Kaur <mka0019@gmail.com>
2020-12-20 22:05:08 -0800

    sass test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Main from './components/MainComponent';
 import { BrowserRouter } from 'react-router-dom';
-import './App.css';
+import './App.scss';
 
 
 
--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -71,27 +71,3 @@ class Header extends Component {
 
 export default Header;
 
-
-// import React, { Component } from 'react';
-// import Main from './components/MainComponent';
-// import { BrowserRouter } from 'react-router-dom';
-// import './App.css';
-
-
-
-// class App extends Component {
-//   render() {
-//       return (
-//         <BrowserRouter>
-//           <div className="App">
-//             <Main />
-//             {/* We moved the visual content to another component > MainComponent.js
-//                 so that this file will just have the essential container that will pull the content/files in */}
-//           </div>
-//         </BrowserRouter>
-//       );
-//   }
-// }
-
-
-// export default App;
