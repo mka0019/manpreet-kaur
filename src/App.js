ca1db447785a76fc5fe653dac5f99f5409b3b237
Manpreet Kaur <mka0019@gmail.com>
2020-12-20 21:53:52 -0800

    header test again

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
 
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+      return (
+        <BrowserRouter>
+          <div className="App">
+            <Main />
+            {/* We moved the visual content to another component > MainComponent.js
+                so that this file will just have the essential container that will pull the content/files in */}
+          </div>
+        </BrowserRouter>
+      );
+  }
 }
+
 
 export default App;
